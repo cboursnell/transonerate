2124974493d4b7896c275e3fccb252242a4547a8
Chris Boursnell <cboursnell@gmail.com>
2014-09-05 17:02:01 +0100

    basic commands for tophat and cufflinks

--- lib/transonerate/tophat.rb
+++ lib/transonerate/tophat.rb
@@ -1,6 +1,36 @@
+require 'open3'
+
 module Transonerate
 
   class Tophat
+
+    def initialize left, right, genome, gtf
+
+    end
+
+    def create_index
+      build = "#{@build_path} #{@genome} #{@index}"
+    end
+
+    def align_reads
+      tophat_cmd = "#{@tophat_path}"
+      tophat_cmd << " -o #{@outputdir} " # options
+      tophat_cmd << " -p #{@threads} " # options
+      # tophat_cmd << " --phred64-quals "  #
+      tophat_cmd << " --no-convert-bam "  # Output is <output_dir>/accepted_hit.sam)
+      tophat_cmd << " --b2-very-sensitive "
+      tophat_cmd << " -G #{@existing_gff} "
+      tophat_cmd << " #{@index} "
+      tophat_cmd << " #{@left} "
+      tophat_cmd << " #{@right} "
+    end
+
+    def create_new_gtf
+      cufflinks_cmd = "#{@cufflinks_path}"
+      cufflinks_cmd << " -o #{@outputdir} "
+      cufflinks_cmd << " -p #{@threads} "
+      cufflinks_cmd << " #{@outputdir}/accepted_hits.sam "
+    end
 
   end
 
